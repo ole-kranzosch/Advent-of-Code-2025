340d3d90ba4a142f8414dc57106674ee7e170b43
ole-kranzosch <olekranzo@gmail.com>
2025-12-12 09:50:34 +0200

    Added Day 12 Solution

--- Day12/Code.m
+++ Day12/Code.m
@@ -1,7 +1,26 @@
 clear; close all;
 %% Read input
 inp = readlines("input.txt");
-sol1 = NaN;
+shapes = inp(1:30);
+for i = 1:5
+    st = 5*(i-1)+2;
+    shape_mat(:,:,i) = char(shapes(st:st+2))-'0';
+end
+shape_mat(shape_mat==-13)=1;
+shape_mat(shape_mat==-2)=0;
+regions = inp(31:end);
+fits = 0;
+for i = 1:size(regions,1)
+    row = regions(i);
+    splt_row = strsplit(row,':');
+    grid = double(strsplit(splt_row(1),'x'));
+    grid = zeros(grid(2),grid(1));
+    presents = str2num(splt_row(2)); %#ok<ST2NM>
+    if sum(presents)*7<=size(grid,1)*size(grid,2)
+        fits = fits +1;
+    end
+end
+sol1 = fits;
 fprintf('Part 1 Solution: %d \n', sol1);
 %% Part 2
 sol2 = NaN;

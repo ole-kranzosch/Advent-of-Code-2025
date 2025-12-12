ca96e873f9b578233f16918ee93fa6c8d4382ed1
ole-kranzosch <olekranzo@gmail.com>
2025-12-12 10:00:02 +0200

    Removed unnecessary lines

--- Day12/Code.m
+++ Day12/Code.m
@@ -1,22 +1,14 @@
 clear; close all;
 %% Read input
 inp = readlines("input.txt");
-shapes = inp(1:30);
-for i = 1:5
-    st = 5*(i-1)+2;
-    shape_mat(:,:,i) = char(shapes(st:st+2))-'0';
-end
-shape_mat(shape_mat==-13)=1;
-shape_mat(shape_mat==-2)=0;
 regions = inp(31:end);
 fits = 0;
 for i = 1:size(regions,1)
     row = regions(i);
     splt_row = strsplit(row,':');
     grid = double(strsplit(splt_row(1),'x'));
-    grid = zeros(grid(2),grid(1));
     presents = str2num(splt_row(2)); %#ok<ST2NM>
-    if sum(presents)*7<=size(grid,1)*size(grid,2)
+    if sum(presents)*7<=grid(1)*grid(2)
         fits = fits +1;
     end
 end
